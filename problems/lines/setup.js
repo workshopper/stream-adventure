772ada0b163bb8051c8db8007232211cedff5183
James Halliday <mail@substack.net>
2013-06-17 03:24:08 -0700

    fix the setup script for the lines demo

--- problems/lines/setup.js
+++ problems/lines/setup.js
@@ -4,11 +4,12 @@ var fs = require('fs');
 var data = fs.readFileSync(__dirname + '/finnegans_wake.txt');
 
 module.exports = function () {
+    var tr = through();
     var chunks = chunky(data);
     var iv = setInterval(function () {
         var buf = chunks.shift();
         if (!buf) { clearInterval(iv); tr.queue(null) }
-        else this.queue(buf)
+        else tr.queue(buf)
     }, 50);
     
     return { args: [], stdin: tr };
