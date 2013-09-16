69ce986eb36bb37c6bc20a7f26ac441bc022dcda
Daniel Saewitz <switz213@gmail.com>
2013-09-16 18:56:46 -0400

    Support coffeescript

--- bin/verify.js
+++ bin/verify.js
@@ -3,6 +3,7 @@ var tuple = require('tuple-stream');
 var through = require('through');
 var split = require('split');
 var path = require('path');
+var coffee = require('coffee-script');
 
 var x256 = require('x256');
 
@@ -20,26 +21,34 @@ var COLORS = (function () {
 COLORS.RESET = '\x1b[00m';
 
 module.exports = function (acmd, bcmd, opts) {
+    var a;
     if (!opts) opts = {};
-    var a = spawn(process.execPath, acmd);
+
+    if (coffee.helpers.isCoffee(acmd)) {
+        a = spawn(__dirname + '/../node_modules/coffee-script/bin/coffee', acmd);
+    }
+    else {
+        a = spawn(process.execPath, acmd);
+    }
+
     if (opts.run) {
         (opts.a || a.stdout).pipe(process.stdout);
         if (a.stderr) a.stderr.pipe(process.stderr);
         return opts.a || a.stdin;
     }
-    
+
     var b = spawn(process.execPath, bcmd);
     var c = compare(opts.a || a.stdout, opts.b || b.stdout, opts);
-    
+
     c.on('pass', function () { kill(); tr.emit('pass') });
     c.on('fail', function () { kill(); tr.emit('fail') });
-    
+
     var tr = through();
     tr.pipe(opts.a || a.stdin);
     tr.pipe(opts.b || b.stdin);
-    
+
     return tr;
-    
+
     function kill () {
         if (a.kill) a.kill();
         if (b.kill) b.kill();
@@ -49,25 +58,25 @@ module.exports = function (acmd, bcmd, opts) {
 function compare (actual, expected, opts) {
     var equal = true;
     var output = through(write, end).pause();
-    
+
     output.queue(COLORS.RESET);
-    
+
     if (!opts.long) {
         output.queue(wrap('ACTUAL', 30) + '     EXPECTED\n');
         output.queue(wrap('------', 30) + '     --------\n');
     }
-    
+
     tuple(actual.pipe(split()), expected.pipe(split()))
         .pipe(output)
         .pipe(process.stdout)
     ;
     output.resume();
     return output;
-    
+
     function write (pair) {
         var eq = pair[0] === pair[1];
         equal = equal && eq;
-        
+
         if (opts.long) {
             this.queue('ACTUAL:   '
                 + COLORS[eq ? 'PASS' : 'FAIL']
@@ -88,7 +97,7 @@ function compare (actual, expected, opts) {
             );
         }
     }
-    
+
     function end () {
         output.queue(COLORS.RESET);
         this.queue(null);
