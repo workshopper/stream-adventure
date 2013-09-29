c1731afb96ab1f42f84a1a2d0be5a088a59beac1
Jeff Paine <jeffreyalanpaine@gmail.com>
2013-09-28 21:32:18 -0400

    Remove duplicative line

--- problems/meet_pipe/setup.js
+++ problems/meet_pipe/setup.js
@@ -6,7 +6,6 @@ module.exports = function () {
     var file = path.resolve(__dirname, 'data.txt');
     var data = '';
     
-    var data = '';
     for (var i = 0; i < 10; i++) {
         var alien = aliens[Math.floor(Math.random() * aliens.length)];
         data += alien + '\n';
