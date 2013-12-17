42219555864c05d8ecfe6190ec94eac3efec3cac
James Halliday <mail@substack.net>
2013-12-16 21:14:04 -0800

    updated the http server to use a function a and b

--- problems/http_server/setup.js
+++ problems/http_server/setup.js
@@ -2,58 +2,56 @@ var through = require('through');
 var duplexer = require('duplexer');
 var hyperquest = require('hyperquest');
 var words = require('./words.json');
+var spawn = require('child_process').spawn;
 
 module.exports = function (opts) {
-    var inputA = through().pause();
-    var inputB = through().pause();
     var aPort = Math.floor(Math.random() * 40000 + 10000);
     var bPort = aPort + 1;
     
-    var outputA = through();
-    var outputB = through();
-    
-    setTimeout(function () {
-        var hqa = hyperquest.post('http://localhost:' + aPort);
-        hqa.on('error', function (err) {
-            console.error('ACTUAL SERVER ' + err.stack);
-        });
-        inputA.pipe(hqa).pipe(outputA);
-        
-        if (!opts.run) {
-            var hqb = hyperquest.post('http://localhost:' + bPort)
-            hqb.on('error', function (err) {
-                console.error('EXPECTED SERVER ' + err.stack);
-            });
-            inputB.pipe(hqb).pipe(outputB);
-        }
-        
-        inputA.resume();
-        inputB.resume();
-    }, 500);
-    
     var offset = Math.floor(words.length*Math.random());
-    var count = 0;
-    var iv = setInterval(function () {
-        var w = words[(offset+count)%words.length] + '\n';
-        inputA.write(w);
-        inputB.write(w);
-        
-        if (++count === 20) {
-            clearInterval(iv);
-            inputA.end();
-            inputB.end();
-            a.emit('kill');
-            b.emit('kill');
-        }
-    }, 50);
-    
-    var a = duplexer(inputA, outputA);
-    var b = duplexer(inputB, outputB);
+    var data = [];
+    for (var i = 0; i < 20; i++) {
+        var w = words[(offset+i)%words.length] + '\n';
+        data.push(w);
+    }
+    var close = [];
     return {
-        a: a,
-        aArgs: [ aPort ],
-        b: b,
-        bArgs: [ bPort ],
-        showStdout: true
+        a: function (args) { return run(args, aPort) },
+        b: function (args) { return run(args, bPort) },
+        close: function () { close.forEach(function (f) { f() }) }
     };
+    
+    function run (args, port) {
+        var ps = spawn(process.execPath, args.concat(port));
+        ps.stderr.pipe(process.stderr);
+        close.push(function () { ps.kill() });
+        
+        if (opts.run) {
+            ps.stdout.pipe(process.stdout);
+        }
+        var stream = check(aPort);
+        return stream;
+    }
+    
+    function check (port, cb) {
+        var input = through();
+        var output = through();
+        setTimeout(function () {
+            var hqa = hyperquest.post('http://localhost:' + port);
+            hqa.on('error', function (err) {
+                console.error('ACTUAL SERVER ' + err.stack);
+            });
+            input.pipe(hqa).pipe(output);
+            
+            var chunks = data.slice();
+            var iv = setInterval(function () {
+                if (chunks.length === 0) {
+                    clearInterval(iv);
+                    input.end();
+                }
+                else input.write(chunks.shift());
+            });
+        }, 500);
+        return duplexer(input, output);
+    }
 };
