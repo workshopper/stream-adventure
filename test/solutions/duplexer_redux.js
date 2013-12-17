6267f0cd1d679a293840b81514fe696326623c53
James Halliday <mail@substack.net>
2013-12-16 22:14:18 -0800

    fix the stream-combiner version, add duplex_redux to the test suite

---
+++ test/solutions/duplexer_redux.js
@@ -0,0 +1,15 @@
+var duplexer = require('duplexer');
+var through = require('through');
+
+module.exports = function (counter) {
+    var counts = {};
+    var input = through(write, end);
+    return duplexer(input, counter);
+    
+    function write (row) {
+        counts[row.country] = (counts[row.country] || 0) + 1;
+    }
+    function end () {
+        counter.setCounts(counts);
+    }
+};
