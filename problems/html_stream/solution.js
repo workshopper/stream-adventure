33d9fea4b8f8e90090a851c7fb7f367856cc5f34
Morgan Cheng <morgan.chengmo@gmail.com>
2014-08-01 18:14:29 +0800

    Update  problems/html_stream/solution.js
    Original solution can only handle one DOM element of class 'loud'. The updated version can update all elements of class 'loud'.

--- problems/html_stream/solution.js
+++ problems/html_stream/solution.js
@@ -2,9 +2,12 @@ var trumpet = require('trumpet');
 var through = require('through');
 var tr = trumpet();
 
-var loud = tr.select('.loud').createStream();
-loud.pipe(through(function (buf) {
-    this.queue(buf.toString().toUpperCase());
-})).pipe(loud);
+tr.selectAll('.loud', function(elem) {
+    var loud = elem.createStream();
+    loud.pipe(through(function(buf) {
+        this.queue(buf.toString().toUpperCase());
+    }))
+    .pipe(loud);
+});
 
 process.stdin.pipe(tr).pipe(process.stdout);
