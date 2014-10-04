11136b583539602fef9112c8085f5b68ea89e258
lucascaro <lucascaro@gmail.com>
2014-10-04 13:40:00 -0700

    Split lines in secretz solution to get just one md5 per line

--- test/solutions/secretz.js
+++ test/solutions/secretz.js
@@ -2,14 +2,18 @@ var crypto = require('crypto');
 var tar = require('tar');
 var zlib = require('zlib');
 var through = require('through');
+var split = require('split');
 
 var parser = tar.Parse();
 parser.on('entry', function (e) {
     if (e.type !== 'File') return;
-    
+
     var h = crypto.createHash('md5', { encoding: 'hex' });
-    e.pipe(h).pipe(through(null, end)).pipe(process.stdout);
-    
+    e.pipe(h)
+      .pipe(split())
+      .pipe(through(null, end))
+      .pipe(process.stdout);
+
     function end () { this.queue(' ' + e.path + '\n') }
 });
 
