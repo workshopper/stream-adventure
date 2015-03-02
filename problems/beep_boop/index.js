5c89301d2e9d9ab766f6ee9a2ea3300dd52eca8a
James Halliday <mail@substack.net>
2015-03-01 22:31:45 -0800

    porting to adventure, beep_boop ported

---
+++ problems/beep_boop/index.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+var verify = require('adventure-verify');
+var concat = require('concat-stream');
+var spawn = require('child_process').spawn;
+
+exports.problem = fs.createReadStream(path.join(__dirname, 'problem.txt'));
+exports.solution = fs.createReadStream(path.join(__dirname, 'solution.js'));
+
+exports.verify = verify({ modeReset: true }, function (args, t) {
+    t.plan(3);
+    
+    t.equal(args.length, 1, 'stream-adventure verify YOURFILE.js')
+    
+    var ps = spawn(process.execPath, args);
+    ps.stderr.pipe(process.stderr);
+    
+    ps.stdout.pipe(concat(function (body) {
+        t.equal(body.toString(), 'beep boop\n');
+    }));
+    ps.on('exit', function (code) {
+        t.equal(code, 0, 'successful exit code');
+    });
+});
