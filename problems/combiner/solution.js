13625ed5149ff4e395df761a996c1f4f69ea96a9
James Halliday <mail@substack.net>
2015-03-02 01:53:36 -0800

    upgraded combiner

--- problems/combiner/solution.js
+++ problems/combiner/solution.js
@@ -1,33 +1,35 @@
-var combine = require('stream-combiner');
-var through = require('through2');
-var split = require('split');
-var zlib = require('zlib');
+// Here's the reference solution:
 
-module.exports = function () {
-    var grouper = through(write, end);
-    var current;
-    
-    function write (line, _, next) {
-        if (line.length === 0) return next();
-        var row = JSON.parse(line);
-        
-        if (row.type === 'genre') {
-            if (current) {
-                this.push(JSON.stringify(current) + '\n');
-            }
-            current = { name: row.name, books: [] };
-        }
-        else if (row.type === 'book') {
-            current.books.push(row.name);
-        }
-        next();
-    }
-    function end (next) {
-        if (current) {
-            this.push(JSON.stringify(current) + '\n');
-        }
-        next();
-    }
-    
-    return combine(split(), grouper, zlib.createGzip());
-};
+  var combine = require('stream-combiner');
+  var through = require('through2');
+  var split = require('split');
+  var zlib = require('zlib');
+
+  module.exports = function () {
+      var grouper = through(write, end);
+      var current;
+      
+      function write (line, _, next) {
+          if (line.length === 0) return next();
+          var row = JSON.parse(line);
+          
+          if (row.type === 'genre') {
+              if (current) {
+                  this.push(JSON.stringify(current) + '\n');
+              }
+              current = { name: row.name, books: [] };
+          }
+          else if (row.type === 'book') {
+              current.books.push(row.name);
+          }
+          next();
+      }
+      function end (next) {
+          if (current) {
+              this.push(JSON.stringify(current) + '\n');
+          }
+          next();
+      }
+      
+      return combine(split(), grouper, zlib.createGzip());
+  };
