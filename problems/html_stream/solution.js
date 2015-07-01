bad7cd0e28cebd243ed20601aa68cf474f346b4b
dbencic <dragan.bencic@gmail.com>
2015-07-02 00:02:11 +0200

    Support for multiple occurrences of 'loud' class
    This solution supports multiple elements with 'loud' class. Previous solution transforms only first one.

--- problems/html_stream/solution.js
+++ problems/html_stream/solution.js
@@ -4,10 +4,12 @@
   var through = require('through2');
   var tr = trumpet();
   
-  var loud = tr.select('.loud').createStream();
-  loud.pipe(through(function (buf, _, next) {
+  tr.selectAll('.loud', function(loud) {
+  	var stream = loud.createStream();
+  	stream.pipe(through(function (buf, _, next) {
       this.push(buf.toString().toUpperCase());
       next();
-  })).pipe(loud);
+  	})).pipe(stream);
+  });
   
   process.stdin.pipe(tr).pipe(process.stdout);
