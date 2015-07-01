055cb9a531df6c8c5e7dc0a528c698fd98e9821c
Dragan Bencic <dragan.bencic@gmail.com>
2015-07-02 00:11:52 +0200

    Solution that supports multiple 'loud' tags

--- problems/html_stream/solution.js
+++ problems/html_stream/solution.js
@@ -5,11 +5,11 @@
   var tr = trumpet();
   
   tr.selectAll('.loud', function(loud) {
-  	var stream = loud.createStream();
-  	stream.pipe(through(function (buf, _, next) {
+    var stream = loud.createStream();
+    stream.pipe(through(function (buf, _, next) {
       this.push(buf.toString().toUpperCase());
       next();
-  	})).pipe(stream);
+    })).pipe(stream);
   });
   
   process.stdin.pipe(tr).pipe(process.stdout);
