71740fd4b8465564f36b20d153cd54c49f86e60b
Lars-Magnus Skog <ralphtheninja@riseup.net>
2016-01-24 22:57:02 +0100

    fix duplexer_redux test
    duplexer() needs { objectMode: true } as well so it
    can pass down options to `stream.Duplex`

--- test/solutions/duplexer_redux.js
+++ test/solutions/duplexer_redux.js
@@ -4,7 +4,7 @@ var through = require('through2').obj;
 module.exports = function (counter) {
     var counts = {};
     var input = through(write, end);
-    return duplexer(input, counter);
+    return duplexer({objectMode: true}, input, counter);
     
     function write (row, _, next) {
         counts[row.country] = (counts[row.country] || 0) + 1;
