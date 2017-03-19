ca4ef4053b7ef5edc4c342562f7265acac2eefb2
Kalin Chernev <kalin.chernev@gmail.com>
2017-03-19 17:44:38 +0000

    refactor(meet-pipe): reuse workshopper-exercise

--- exercises/meet_pipe/exercise.js
+++ exercises/meet_pipe/exercise.js
@@ -1,49 +1,15 @@
-var fs = require('fs');
-var path = require('path');
-var verify = require('adventure-verify');
-var concat = require('concat-stream');
+var exercise = require('workshopper-exercise')()
+var filecheck = require('workshopper-exercise/filecheck')
+var execute = require('workshopper-exercise/execute')
+var comparestdout = require('workshopper-exercise/comparestdout')
 
-var spawn = require('child_process').spawn;
-var tmpdir = require('osenv').tmpdir();
-var aliens = require('./aliens.json');
+// checks that the submission file actually exists
+exercise = filecheck(exercise)
 
-exports.problem = fs.createReadStream(path.join(__dirname, 'problem.txt'));
-exports.solution = fs.createReadStream(path.join(__dirname, 'solution.js'));
+// execute the solution and submission in parallel with spawn()
+exercise = execute(exercise)
 
+// compare stdout of solution and submission
+exercise = comparestdout(exercise)
 
-var lines = [];
-for (var i = 0; i < 10; i++) {
-    lines.push(aliens[Math.floor(Math.random() * aliens.length)]);
-}
-var data = lines.join('\n') + '\n';
-
-exports.verify = verify({ modeReset: true }, function (args, t) {
-    t.plan(3);
-    
-    t.equal(args.length, 1, 'stream-adventure verify YOURFILE.js')
-    
-    var file = path.resolve(tmpdir, 'meet-pipe-data.txt');
-    fs.writeFileSync(file, data);
-    
-    var ps = spawn(process.execPath, [ args[0], file ]);
-    ps.stderr.pipe(process.stderr);
-    
-    ps.stdout.pipe(concat(function (body) {
-        t.deepEqual(body.toString(), data);
-    }));
-    ps.on('exit', function (code) {
-        t.equal(code, 0, 'successful exit code');
-    });
-});
-
-exports.run = function (args) {
-    var file = path.resolve(tmpdir, 'meet-pipe-data.txt');
-    fs.writeFileSync(file, data);
-    
-    var ps = spawn(process.execPath, [ args[0], file ]);
-    ps.stderr.pipe(process.stderr);
-    ps.stdout.pipe(process.stdout);
-    ps.once('exit', function (code) {
-        if (code) process.exit(code)
-    });
-};
+module.exports = exercise
--- exercises/meet_pipe/solution/solution.js
+++ exercises/meet_pipe/solution/solution.js
@@ -1,5 +1,3 @@
-// Here's the reference solution:  
-  
-  var fs = require('fs');
-  var file = process.argv[2];
-  fs.createReadStream(file).pipe(process.stdout);
+var fs = require('fs');
+var file = process.argv[2];
+fs.createReadStream(file).pipe(process.stdout);
