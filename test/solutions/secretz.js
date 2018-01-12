e891881744cdc3c743c3a360a71b60c44556607f
Finn Pauls <derfinn@gmail.com>
2018-01-12 17:21:20 +0100

    update solution everywhere, update dependency, small change

--- test/solutions/secretz.js
+++ test/solutions/secretz.js
@@ -5,8 +5,8 @@ var concat = require('concat-stream');
 
 var parser = new tar.Parse();
 parser.on('entry', function (e) {
-    if (e.type !== 'File') e.resume();
-    
+    if (e.type !== 'File') return e.resume();
+
     var h = crypto.createHash('md5', { encoding: 'hex' });
     e.pipe(h).pipe(concat(function (hash) {
         console.log(hash + ' ' + e.path);
