8352fe401c66c375e836768e9b41be7da36ab392
César Carruitero <ccarruitero@protonmail.com>
2020-04-10 12:56:44 -0500

    refactor: input_output exercise

--- problems/beep_boop/index.js
+++ problems/beep_boop/index.js
@@ -1,19 +1,10 @@
 const path = require('path')
-
-let exercise = require('workshopper-exercise')()
-const filecheck = require('workshopper-exercise/filecheck')
-const execute = require('workshopper-exercise/execute')
 const comparestdout = require('workshopper-exercise/comparestdout')
-const wrappedexec = require('workshopper-wrappedexec')
 
-exercise = filecheck(exercise)
-
-exercise = execute(exercise)
+let exercise = require('../../lib/exercise')
 
 exercise.solution = path.join(__dirname, 'solution.js')
 
 exercise = comparestdout(exercise)
-
-exercise = wrappedexec(exercise)
 
 module.exports = exercise
--- problems/input_output/index.js
+++ problems/input_output/index.js
@@ -1,60 +1,42 @@
-var fs = require('fs')
-var path = require('path')
-var verify = require('adventure-verify')
-var concat = require('concat-stream')
+const path = require('path')
+const comparestdout = require('workshopper-exercise/comparestdout')
 
-var spawn = require('child_process').spawn
-var aliens = require('./aliens.json')
+let exercise = require('../../lib/exercise')
 
-exports.problem = fs.createReadStream(path.join(__dirname, 'problem.txt'))
-exports.solution = fs.createReadStream(path.join(__dirname, 'solution.js'))
+exercise.solution = path.join(__dirname, 'solution.js')
 
-var expected = ''; var input = []
-for (var i = 0; i < 10; i++) {
-  var alien = aliens[Math.floor(Math.random() * aliens.length)]
-  expected += alien + '\n'
-  input.push(alien + '\n')
-}
+const aliens = require('./aliens.json')
 
-exports.verify = verify({ modeReset: true }, function (args, t) {
-  t.plan(3)
-
-  t.equal(args.length, 1, 'stream-adventure verify YOURFILE.js')
-  var ps = spawn(process.execPath, args)
-  ps.stderr.pipe(process.stderr)
-
-  ps.stdout.pipe(concat(function (body) {
-    t.equal(body.toString(), expected)
-  }))
-
-  ps.on('exit', function (code) {
-    t.equal(code, 0, 'successful exit code')
-  })
-
-  var iv = setInterval(function () {
+function messageStdin (submissionStdin, solutionStdin, input) {
+  const iv = setInterval(function () {
     if (input.length) {
-      ps.stdin.write(input.shift())
+      const msg = input.shift()
+      submissionStdin.write(msg)
+      if (solutionStdin) solutionStdin.write(msg)
     } else {
       clearInterval(iv)
-      ps.stdin.end()
+      submissionStdin.end()
+      if (solutionStdin) solutionStdin.end()
     }
   }, 50)
+}
+
+exercise.addProcessor(function (mode, callback) {
+  const input = []
+  for (let i = 0; i < 10; i++) {
+    const alien = aliens[Math.floor(Math.random() * aliens.length)]
+    input.push(`${alien}\n`)
+  }
+
+  const solutionStdin = (mode === 'verify') ? this.solutionChild.stdin : null
+
+  messageStdin(this.submissionChild.stdin, solutionStdin, input)
+
+  process.nextTick(function () {
+    callback(null, true)
+  })
 })
 
-exports.run = function (args) {
-  var ps = spawn(process.execPath, args)
-  ps.stderr.pipe(process.stderr)
-  ps.stdout.pipe(process.stdout)
-  ps.once('exit', function (code) {
-    if (code) process.exit(code)
-  })
+exercise = comparestdout(exercise)
 
-  var iv = setInterval(function () {
-    if (input.length) {
-      ps.stdin.write(input.shift())
-    } else {
-      clearInterval(iv)
-      ps.stdin.end()
-    }
-  }, 50)
-}
+module.exports = exercise
