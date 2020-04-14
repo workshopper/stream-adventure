fc11bc151ed81d72ea1532fed373b81ae61de87f
César Carruitero <ccarruitero@protonmail.com>
2020-04-14 16:32:27 -0500

    refactor: duplexer_redux exercise

--- problems/duplexer/index.js
+++ problems/duplexer/index.js
@@ -1,10 +1,6 @@
 const path = require('path')
-const stream = require('stream')
 
-let exercise = require('workshopper-exercise')()
-const filecheck = require('workshopper-exercise/filecheck')
-const stdinProcessor = require('../../lib/stdinProcessor')
-const comparestdout = require('workshopper-exercise/comparestdout')
+const exercise = require('../../lib/duplexExercise')
 
 const words = [
   'beetle',
@@ -41,51 +37,11 @@ const getInput = () => {
   return input
 }
 
-exercise = filecheck(exercise)
-
 exercise.solution = path.join(__dirname, 'solution.js')
 exercise.inputStdin = getInput()
 
-exercise.addSetup(function (mode, callback) {
-  this.submissionFn = require(path.resolve(this.args[0]))
-
-  if (typeof this.submissionFn !== 'function') {
-    this.emit('fail', this.__('fail.invalid_export'))
-  }
-  this.solutionFn = require(this.solution)
-
-  process.nextTick(callback)
-})
-
-exercise.addProcessor(function (mode, callback) {
-  const n = 1 + Math.floor(Math.random() * 25)
-  const cmd = path.resolve(__dirname, 'command.js')
-
-  this.submissionChild = this.submissionFn(process.execPath, [cmd, n])
-  this.submissionStdout = this.submissionChild
-  this.submissionChild.stdin = this.submissionChild
-
-  // We need a readable stream for `submissionChild.stderr`, since is piped to
-  // `process.stderr` later by comparestdout
-  // https://github.com/workshopper/workshopper-exercise/blob/master/comparestdout.js#L37
-  const stderr = new stream.Readable({ objectMode: true })
-  stderr._read = function _read (n) { }
-
-  this.submissionChild.stderr = stderr
-
-  if (mode === 'verify') {
-    this.solutionChild = this.solutionFn(process.execPath, [cmd, n])
-    this.solutionStdout = this.solutionChild
-    this.solutionChild.stdin = this.solutionChild
-  }
-
-  process.nextTick(function () {
-    callback(null, true)
-  })
-})
-
-exercise = stdinProcessor(exercise)
-
-exercise = comparestdout(exercise)
+const n = 1 + Math.floor(Math.random() * 25)
+const cmd = path.resolve(__dirname, 'command.js')
+exercise.execArgs = [cmd, n]
 
 module.exports = exercise
