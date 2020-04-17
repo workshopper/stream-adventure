7fc9773c01e7ba0e0f12b4b0ed28281653ed45f8
César Carruitero <ccarruitero@protonmail.com>
2020-04-17 13:40:47 -0500

    refactor: crypt exercise

--- problems/crypt/index.js
+++ problems/crypt/index.js
@@ -1,52 +1,39 @@
-var fs = require('fs')
-var path = require('path')
-var verify = require('adventure-verify')
-var crypto = require('crypto')
-var spawn = require('child_process').spawn
-var concat = require('concat-stream')
-var words = require('./words.json')
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
 
-exports.problem = fs.createReadStream(path.join(__dirname, 'problem.txt'))
-exports.solution = fs.createReadStream(path.join(__dirname, 'solution.js'))
+const comparestdout = require('workshopper-exercise/comparestdout')
 
-var file = path.join(__dirname, 'finnegans_wake.txt')
+let exercise = require('../../lib/exercise')
+const words = require('./words.json')
 
-exports.verify = verify({ modeReset: true }, function (args, t) {
-  t.plan(4)
-  t.equal(args.length, 1, 'stream-adventure verify YOURFILE.js')
+exercise.solution = path.join(__dirname, 'solution.js')
 
-  var pw = words[Math.floor(Math.random() * words.length)]
-  var key = crypto.createHash('md5').update(pw).digest('hex')
-  var iv = crypto.randomBytes(8).toString('hex')
-  var ps = spawn(process.execPath, [args[0], key, iv])
-  ps.once('exit', function (code) {
-    t.equal(code, 0, 'successful exit code')
-  })
-  ps.stderr.pipe(process.stderr)
-  ps.stdout.pipe(concat(function (body) {
-    fs.readFile(file, function (err, expected) {
-      t.ifError(err)
-      t.equal(body.toString(), expected.toString())
-    })
-  }))
+const file = path.join(__dirname, 'finnegans_wake.txt')
 
-  var enc = crypto.createCipheriv('aes256', key, iv)
-  if (!enc.pipe) {
-    t.fail('Your version of node is too old to play this level. ' +
-            'Please upgrade to node >= 0.10.'
-    )
-  }
+exercise.addSetup(function (mode, callback) {
+  const pw = words[Math.floor(Math.random() * words.length)]
+  const key = crypto.createHash('md5').update(pw).digest('hex')
+  const iv = crypto.randomBytes(8).toString('hex')
+  this.submissionCipher = crypto.createCipheriv('aes256', key, iv)
+  this.solutionCipher = crypto.createCipheriv('aes256', key, iv)
 
-  fs.createReadStream(file).pipe(enc).pipe(ps.stdin)
+  this.submissionArgs.push(key, iv)
+  this.solutionArgs.push(key, iv)
+
+  process.nextTick(callback)
 })
 
-exports.run = function (args) {
-  var pw = words[Math.floor(Math.random() * words.length)]
-  var key = crypto.createHash('md5').update(pw).digest('hex')
-  var iv = crypto.randomBytes(8).toString('hex')
-  var ps = spawn(process.execPath, [args[0], key, iv])
-  ps.stderr.pipe(process.stderr)
-  ps.stdout.pipe(process.stdout)
-  var enc = crypto.createCipheriv('aes256', key, iv)
-  fs.createReadStream(file).pipe(enc).pipe(ps.stdin)
-}
+exercise.addProcessor(function (mode, callback) {
+  fs.createReadStream(file).pipe(this.submissionCipher).pipe(this.submissionChild.stdin)
+
+  if (mode === 'verify') {
+    fs.createReadStream(file).pipe(this.solutionCipher).pipe(this.solutionChild.stdin)
+  }
+
+  process.nextTick(callback)
+})
+
+exercise = comparestdout(exercise)
+
+module.exports = exercise
