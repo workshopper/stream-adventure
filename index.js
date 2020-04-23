29c9e85a1a7f71e8145a7e5cb7cc42ec5f50764b
César Carruitero <ccarruitero@protonmail.com>
2020-04-22 19:15:31 -0500

    refactor: use workshopper-adventure core

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-var path = require('path')
-var adventure = require('workshopper-adventure/adventure')
-var shop = adventure({
+const path = require('path')
+const workshopper = require('workshopper-adventure')
+const util = require('workshopper-adventure/util')
+
+const exerciseDir = path.join(__dirname, './problems')
+const shop = workshopper({
   name: 'stream-adventure',
   title: 'STREAM ADVENTURE',
-  exerciseDir: path.join(__dirname, './problems'),
+  exerciseDir,
   header: require('workshopper-adventure/default/header'),
   footer: require('workshopper-adventure/default/footer'),
   fail: require('workshopper-adventure/default/fail'),
   pass: require('workshopper-adventure/default/pass'),
-  defaultOutputType: 'md',
   appDir: __dirname
 })
 
 require('./menu.json').forEach(function (name) {
-  if (/^!/.test(name)) return
-  var d = name.toLowerCase().replace(/\W+/g, '_')
-  var dir = path.join(__dirname, './problems', d)
-  shop.add(name, function () {
-    return require(dir)
-  })
-})
-shop.on('fail', function () {
-  process.exit(1)
+  const dir = util.dirFromName(exerciseDir, name)
+  const exerciseFile = path.join(dir, './index.js')
+  shop.add({ name, dir, exerciseFile })
 })
 
 module.exports = shop
