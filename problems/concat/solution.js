a2fd01a6ce0353874837f2021d6e709c3f21f06d
César Carruitero <ccarruitero@protonmail.com>
2020-06-08 17:04:59 -0500

    fix: stop cheating in concat solution

--- problems/concat/solution.js
+++ problems/concat/solution.js
@@ -4,5 +4,5 @@ var concat = require('concat-stream')
 
 process.stdin.pipe(concat(function (src) {
   var s = src.toString().split('').reverse().join('')
-  console.log(s)
+  process.stdout.write(s)
 }))
--- test/solutions/concat.js
+++ test/solutions/concat.js
@@ -1,5 +1,5 @@
 var concat = require('concat-stream')
 process.stdin.pipe(concat(function (src) {
   var s = src.toString().split('').reverse().join('')
-  console.log(s)
+  process.stdout.write(s)
 }))
