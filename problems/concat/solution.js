41c2a51b5b6fefc329132a812e00452d068a1617
Eugene Maslovich <ehpc@em42.ru>
2020-09-20 20:22:39 +0300

    fix(concat): Add more optimal solution for reference

--- problems/concat/solution.js
+++ problems/concat/solution.js
@@ -3,4 +3,5 @@ const concat = require('concat-stream')
 process.stdin.pipe(concat(function (src) {
   const s = src.toString().split('').reverse().join('')
   process.stdout.write(s)
+  // Alternatively: process.stdout.write(buf.reverse());
 }))
